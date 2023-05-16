a3d1a9913c3d71cbf976f707d78f8a35c9f8e3df
Erin Thomas <ac.ethomas@chrlogin1.lcrc.anl.gov>
2023-05-16 10:44:26 -0500

    day 1 tutorial adding 'yet-another-channel'

---
+++ polaris/ocean/tests/yet_another_channel/__init__.py
@@ -0,0 +1,18 @@
+from polaris import TestGroup
+from polaris.ocean.tests.yet_another_channel.default import Default
+
+
+class YetAnotherChannel(TestGroup):
+    """
+    A test group for "yet another channel" test cases
+    """
+    def __init__(self, component):
+        """
+        component : polaris.ocean.Ocean
+            the ocean component that this test group belongs to
+        """
+        super().__init__(component=component, name='yet_another_channel')
+
+        for resolution in [1., 4., 10.]:
+            self.add_test_case(
+                Default(test_group=self, resolution=resolution))
